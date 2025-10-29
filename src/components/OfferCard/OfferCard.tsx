723a20524aee9530a2becb5538806874a1a2a7ca
serjo <serjo-study@ya.ru>
2025-10-29 14:34:34 +0300

    refactor: change a to Link tag for OfferCard

--- src/components/OfferCard/OfferCard.tsx
+++ src/components/OfferCard/OfferCard.tsx
@@ -25,64 +25,62 @@ const OfferCard = ({ offer, variant }: OfferCardProps) => {
   const ratingStarsValue: string = `${offer.rating * 100 / 5}%`;
 
   return (
-    <Link to={`/offer/${offer.id}`}>
-      <article
-        className={`${variant.cardName}__card place-card`}
-      // onMouseEnter={handleMouseEnter}
-      // onMouseLeave={handleMouseLeave}
-      >
-        {
-          offer.isPremium && (
-            <div className='place-card__mark'>
-              <span>Premium</span>
-            </div>
-          )
-        }
+    <article
+      className={`${variant.cardName}__card place-card`}
+    // onMouseEnter={handleMouseEnter}
+    // onMouseLeave={handleMouseLeave}
+    >
+      {
+        offer.isPremium && (
+          <div className='place-card__mark'>
+            <span>Premium</span>
+          </div>
+        )
+      }
 
-        <div className={`${variant.cardName}__image-wrapper place-card__image-wrapper`}>
-          <a href='#'>
-            <img
-              className='place-card__image'
-              // src={offer.previewImage}
-              src={'../../../public/img/apartment-01.jpg'} // пока не работает сервер
-              width={variant.widthForImg}
-              height={variant.heightForImg}
-              alt='Place image'
-            />
-          </a>
-        </div>
-        <div className={`${(variant.cardName === 'favorites') ? 'favorites__card-info' : ''} place-card__info`}>
-          <div className='place-card__price-wrapper'>
-            <div className='place-card__price'>
-              <b className='place-card__price-value'>&euro;{offer.price}</b>
-              <span className='place-card__price-text'>&#47;&nbsp;night</span>
-            </div>
-            <button
-              className={`
+      <div className={`${variant.cardName}__image-wrapper place-card__image-wrapper`}>
+        <Link to={`/offer/${offer.id}`}>
+          <img
+            className='place-card__image'
+            // src={offer.previewImage}
+            src={'../../../public/img/apartment-01.jpg'} // пока не работает сервер
+            width={variant.widthForImg}
+            height={variant.heightForImg}
+            alt='Place image'
+          />
+        </Link>
+      </div>
+      <div className={`${(variant.cardName === 'favorites') ? 'favorites__card-info' : ''} place-card__info`}>
+        <div className='place-card__price-wrapper'>
+          <div className='place-card__price'>
+            <b className='place-card__price-value'>&euro;{offer.price}</b>
+            <span className='place-card__price-text'>&#47;&nbsp;night</span>
+          </div>
+          <button
+            className={`
               place-card__bookmark-button 
               ${(offer.isFavorite) ? 'place-card__bookmark-button--active' : ''}
               button`}
-              type='button'
-            >
-              <svg className='place-card__bookmark-icon' width='18' height='19'>
-                <use href='#icon-bookmark'></use>
-              </svg>
-              <span className='visually-hidden'>To bookmarks</span>
-            </button>
-          </div>
-          <div className='place-card__rating rating'>
-            <div className='place-card__stars rating__stars'>
-              <span style={{ width: ratingStarsValue }}></span>
-              <span className='visually-hidden'>{offer.rating}</span>
-            </div>
-          </div>
-          <h2 className='place-card__name'>
-            <a href='#'>{offer.title}</a>
-          </h2>
-          <p className='place-card__type'>{offer.type}</p>
+            type='button'
+          >
+            <svg className='place-card__bookmark-icon' width='18' height='19'>
+              <use href='#icon-bookmark'></use>
+            </svg>
+            <span className='visually-hidden'>To bookmarks</span>
+          </button>
         </div>
-      </article>
-    </Link>
+        <div className='place-card__rating rating'>
+          <div className='place-card__stars rating__stars'>
+            <span style={{ width: ratingStarsValue }}></span>
+            <span className='visually-hidden'>{offer.rating}</span>
+          </div>
+        </div>
+        <h2 className='place-card__name'>
+          <Link to={`/offer/${offer.id}`}>{offer.title}</Link>
+        </h2>
+        <p className='place-card__type'>{offer.type}</p>
+      </div>
+    </article>
   );
 };
 
