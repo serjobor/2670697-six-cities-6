2745b18ff775e66789ca80343f5521d163a760fd
serjo <serjo-study@ya.ru>
2025-12-18 10:29:19 +0300

    refactor: change class style for the host if he is pro

--- src/pages/OfferPage/OfferPage.tsx
+++ src/pages/OfferPage/OfferPage.tsx
@@ -160,7 +160,7 @@ function OfferPage() {
               <div className='offer__host'>
                 <h2 className='offer__host-title'>Meet the host</h2>
                 <div className='offer__host-user user'>
-                  <div className='offer__avatar-wrapper offer__avatar-wrapper--pro user__avatar-wrapper'>
+                  <div className={`offer__avatar-wrapper ${fullOffer.host.isPro ? 'offer__avatar-wrapper--pro' : ''} user__avatar-wrapper`}>
                     <img className='offer__avatar user__avatar' src={fullOffer.host.avatarUrl} width='74' height='74' alt='Host avatar' />
                   </div>
                   <span className='offer__user-name'>
